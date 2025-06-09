87753854584e6751643cc6cc1e57b9dd0bc654ce
premultiply <4681172+premultiply@users.noreply.github.com>
2025-06-10 01:20:25 +0200

    wip

--- src/actions.js
+++ src/actions.js
@@ -1,78 +1,48 @@
 export function setActions(self) {
 	const actions = {}
 
-	actions.rtmpPush = {
-		name: 'RTMP Push',
+	actions.ptMove = {
+		name: 'Pan/Tilt - Move',
 		options: [
 			{
 				type: 'dropdown',
-				label: 'Action',
-				id: 'operation',
-				default: '1',
+				label: 'Direction',
+				id: 'dir',
+				default: '11',
 				choices: [
-					{ id: '1', label: 'Enable' },
-					{ id: '0', label: 'Disable' },
-				],
-			},
-			{
-				type: 'dropdown',
-				label: 'Stream',
-				id: 'stream',
-				default: '0',
-				choices: [
-					{ id: '0', label: 'Stream 1 (Main)' },
-					{ id: '1', label: 'Stream 2 (Sub)' },
-					{ id: '2', label: 'Stream 3 (Sub)' },
-					{ id: '3', label: 'Stream 4 (Sub)' },
+					{ id: '11', label: 'Stop' },
+					{ id: '21', label: '➡ Right' },
+					{ id: '01', label: '⬅ Left' },
+					{ id: '10', label: '⬆ Up' },
+					{ id: '12', label: '⬇ Down' },
+					{ id: '20', label: '⬈ Up Right' },
+					{ id: '00', label: '⬉ Up Left' },
+					{ id: '02', label: '⬋ Down Left' },
+					{ id: '22', label: '⬊ Down Right' },
 				],
 			},
 		],
 		callback: async (action) => {
-			await self.sendCommand(
-				`set_output?input=0&output=${action.options.stream}&rtmp_publish_enable=${action.options.operation}`,
-			)
-		},
-	}
-
-	actions.srtCaller = {
-		name: 'SRT Caller',
-		options: [
-			{
-				type: 'dropdown',
-				label: 'Action',
-				id: 'operation',
-				default: '1',
-				choices: [
-					{ id: '1', label: 'Enable' },
-					{ id: '0', label: 'Disable' },
-				],
-			},
-			{
-				type: 'dropdown',
-				label: 'Stream',
-				id: 'stream',
-				default: '0',
-				choices: [
-					{ id: '0', label: 'Stream 1 (Main)' },
-					{ id: '1', label: 'Stream 2 (Sub)' },
-					{ id: '2', label: 'Stream 3 (Sub)' },
-					{ id: '3', label: 'Stream 4 (Sub)' },
-				],
-			},
-		],
-		callback: async (action) => {
-			await self.sendCommand(
-				`set_output?input=0&output=${action.options.stream}&srt_publish_enable=${action.options.operation}`,
-			)
-		},
-	}
-
-	actions.reboot = {
-		name: 'Reboot',
-		description: 'Reboots the device without any further confirmation',
-		options: [],
-		callback: async (action) => {
-			await self.sendCommand('reboot')
+			if (action.options.dir === '11') {
+				// Stop
+				await self.sendCommand('/gimbal_speed', 'POST', { pitch: 0, yaw: 0, roll: 0, stop: true })
+				//if (self.speedChangeEmitter.listenerCount('ptSpeed')) self.speedChangeEmitter.removeAllListeners('ptSpeed')
+			} else {
+				let arr = Array.from(action.options.dir)
+				let yaw = (parseInt(arr[0]) - 1) * 1
+				let pitch = (parseInt(arr[1]) - 1) * 1
+				await self.sendCommand('/gimbal_speed', 'POST', { pitch, yaw, roll: 0, stop: false })
+				//await self.getPTZ('PTS' + cmdSpeed(pan * self.pSpeed + SPEED_OFFSET) + cmdSpeed(tilt * self.tSpeed + SPEED_OFFSET),)
+				// if (action.options.liveSpeed) {
+				// 	self.speedChangeEmitter.removeAllListeners('ptSpeed').then(
+				// 		self.speedChangeEmitter.on('ptSpeed', async () => {
+				// 			await self.getPTZ(
+				// 				'PTS' + cmdSpeed(pan * self.pSpeed + SPEED_OFFSET) + cmdSpeed(tilt * self.tSpeed + SPEED_OFFSET),
+				// 			)
+				// 		}),
+				// 	)
+				// }
+			}
 		},
 	}
 
--- src/index.js
+++ src/index.js
@@ -9,6 +9,51 @@ import { ConfigFields } from './config.js'
 import WebSocket from 'ws'
 import Queue from 'queue-fifo'
 
+const API_ENDPOINTS = [
+	'/record/control',
+	'/record/bitrate',
+	'/record/encoder',
+	'/record/resolution',
+	'/ndi-rtsp/control',
+	'/ndi-rtsp/bitrate',
+	'/ndi-rtsp/encoder',
+	'/ndi-rtsp/resolution',
+	'/hdmi/output/resolution',
+	//'/usb/mode',
+	'/audio/input/source',
+	'/audio/input/volume',
+	'/audio/input/mute',
+	'/audio/input/enc',
+	'/audio/input/agc',
+	'/ai/workmode',
+	'/ai/trackspeed',
+	//'/ai/gesturecontrol/lockedtarget',
+	//'/ai/gesturecontrol/recording',
+	//'/ai/gesturecontrol/zoom',
+	//'/ai/gesturecontrol/zoomfactor',
+	//'/ai/gesturecontrol/dynamiczoom',
+	//'/ai/gesturecontrol/dynamiczoomdirection',
+	//'/image/hdr/control',
+	//'/image/af/mode', // available from status
+	'/image/af/trackmode',
+	//'/image/af/motorposition', // available from status
+	'/image/af/windowcenter',
+	'/image/exposure/mode',
+	'/image/exposure/auto/mode',
+	'/image/exposure/auto/compensation',
+	'/image/exposure/auto/isorange',
+	'/image/exposure/auto/lock',
+	'/image/exposure/manual/shuttertime',
+	'/image/exposure/manual/iso',
+	'/image/exposure/antiflick/mode',
+	'/image/whitebalance/config',
+	'/image/style/mode',
+	'/ptz/gimbalinvert',
+	//'/ptz/zoom', // from available from status
+	//'/ptz/preset', // from available from status
+	//'/album/filelist',
+]
+
 class ObsbotRestCameraInstance extends InstanceBase {
 	constructor(internal) {
 		super(internal)
@@ -24,7 +69,7 @@ class ObsbotRestCameraInstance extends InstanceBase {
 		this.initWebSocket()
 
 		this.init_variables()
-		//this.init_actions()
+		this.init_actions()
 		//this.init_feedbacks()
 		//this.init_presets()
 
@@ -44,7 +89,7 @@ class ObsbotRestCameraInstance extends InstanceBase {
 		this.disablePolling()
 		this.controller.abort()
 		if (this.ws) {
-			this.ws.close(1000)
+			this.ws.close(1001)
 			delete this.ws
 		}
 		this.updateStatus(InstanceStatus.Disconnected)
@@ -60,168 +105,168 @@ class ObsbotRestCameraInstance extends InstanceBase {
 	}
 
 	enablePolling() {
-		clearInterval(this.pollID)
-		this.pollID = setInterval(() => this.pullData(), 1000)
-		this.log('debug', 'Polling enabled with 1s interval')
+		clearTimeout(this.pollID)
+		this.pollID = setTimeout(() => this.pollData(), 10)
+		this.log('debug', 'Polling enabled')
 	}
 
 	disablePolling() {
-		clearInterval(this.pollID)
+		clearTimeout(this.pollID)
 		this.pollID = null
 		this.log('debug', 'Polling disabled')
 	}
 
 	emptyCache() {
 		return {
-			record: {
-				control: { recording: 'off' },
-				bitrate: { bitrate: 60000000, bitrateLevel: 'high' },
-				encoder: { encoder: 'h264' },
-				resolution: { resolution: '1920X1080P50' },
-			},
-			'ndi-rtsp': {
-				control: { control: 'ndi' },
-				bitrate: { bitrateLevel: 'high' },
-				encoder: { encoder: 'h264' },
-				resolution: { resolution: '1920X1080P50' },
-			},
-			hdmi: {
-				output: {
-					resolution: { resolution: '1920X1080P50' },
-				},
-			},
-			usb: {
-				mode: { mode: 'uvc' },
-			},
-			audio: {
-				input: {
-					source: { source: 'buildIn' },
-					volume: { volume: 80 },
-					mute: { enable: false },
-					enc: { enable: true, level: 'weak' },
-					agc: { enable: true },
-				},
-			},
-			ai: {
-				workmode: { mode: 'none' },
-				trackspeed: { speed: 'standard' },
-				gesturecontrol: {
-					lockedtarget: { enable: false },
-					recording: { enable: false },
-					zoom: { enable: false },
-					zoomfactor: { factor: 1 },
-					dynamiczoom: { enable: false },
-					dynamiczoomdirection: { direction: 'forward' },
-				},
-			},
-			image: {
-				hdr: {
-					control: { control: 'off' },
-				},
-				af: {
-					mode: { mode: 'mf' },
-					trackmode: { mode: 'face' },
-					motorposition: { position: 74 },
-					windowcenter: { x: 0.5, y: 0.5 },
-				},
-				exposure: {
-					mode: { mode: 'auto' },
-					auto: {
-						mode: { mode: 'face' },
-						compensation: { evbias: 0 },
-						isorange: { isomin: 100, isomax: 6400 },
-						lock: { enable: false },
-					},
-					manual: {
-						shuttertime: { shutter: '1/30' },
-						iso: { iso: 100 },
-					},
-					antiflick: {
-						mode: { mode: 'off' },
-					},
-				},
-				whitebalance: {
-					config: { mode: 'manual', temperature: 4000 },
-				},
-				style: {
-					mode: { mode: 'standard', brightness: 50, contrast: 50, hue: 50, saturation: 50, sharpness: 50 },
-				},
-			},
-			ptz: {
-				gimbalinvert: { enable: false },
-				zoom: { enable: 4 },
-				preset: {
-					presetList: [
-						{ id: 0, pitch: 1.64, yaw: -53.3, roll: 0, ratio: 1.51, name: 'UDE=' },
-						{ id: 1, pitch: -8.38, yaw: 4.29, roll: 0, ratio: 2, name: 'SGFsYm5haA==' },
-						{ id: 2, pitch: -12.8, yaw: 11.9, roll: 0, ratio: 1, name: 'U3RlaHRpcw==' },
-						{ id: 3, pitch: 8.93, yaw: 149, roll: 0, ratio: 1, name: 'UDQ=' },
-					],
-				},
-			},
-			status: {
-				preset_cnt: [
-					{
-						id: 0,
-						name: 'UDE=',
-					},
-					{
-						id: 1,
-						name: 'SGFsYm5haA==',
-					},
-					{
-						id: 2,
-						name: 'U3RlaHRpcw==',
-					},
-					{
-						id: 3,
-						name: 'UDQ=',
-					},
-				],
-				ratio: 4.0,
-				iszoom: false,
-				target: 0,
-				capture: false,
-				record: false,
-				af_mode: 3,
-				mf_position: 74,
-				is_active: true,
-				ndi_available: true,
-				rtmp_info: {
-					rtmp_status: 0,
-				},
-				sdcard: {
-					status: 2,
-					total: 15929475072,
-					available: 15713042432,
-					sdcard_speed: 2,
-					avail_time: 2095,
-					format: 2,
-				},
-				device_status: {
-					is_gimbal_online: true,
-					is_ai_online: true,
-					is_battery_online: true,
-					is_lens_online: true,
-					is_tof_online: true,
-					is_sensor_ok: true,
-					is_media_ok: true,
-					is_ble_ok: true,
-					is_usbwifi_attached: false,
-					is_poe_attached: true,
-					is_hdmi_attached: false,
-					is_35mm_attached: false,
-					is_remote_attached: false,
-					is_charge: true,
-					battery_cap: 89,
-					battery_temperature: 23748,
-					battery_temperature_status: 0,
-					cpu_temperature: 0,
-					cpu_temperature_status: 0,
-					lens_temperature: 42,
-					lens_temperature_status: 0,
-				},
-			},
+			// 	record: {
+			// 		control: { recording: 'off' },
+			// 		bitrate: { bitrate: 60000000, bitrateLevel: 'high' },
+			// 		encoder: { encoder: 'h264' },
+			// 		resolution: { resolution: '1920X1080P50' },
+			// 	},
+			// 	'ndi-rtsp': {
+			// 		control: { control: 'ndi' },
+			// 		bitrate: { bitrateLevel: 'high' },
+			// 		encoder: { encoder: 'h264' },
+			// 		resolution: { resolution: '1920X1080P50' },
+			// 	},
+			// 	hdmi: {
+			// 		output: {
+			// 			resolution: { resolution: '1920X1080P50' },
+			// 		},
+			// 	},
+			// 	usb: {
+			// 		mode: { mode: 'uvc' },
+			// 	},
+			// 	audio: {
+			// 		input: {
+			// 			source: { source: 'buildIn' },
+			// 			volume: { volume: 80 },
+			// 			mute: { enable: false },
+			// 			enc: { enable: true, level: 'weak' },
+			// 			agc: { enable: true },
+			// 		},
+			// 	},
+			// 	ai: {
+			// 		workmode: { mode: 'none' },
+			// 		trackspeed: { speed: 'standard' },
+			// 		gesturecontrol: {
+			// 			lockedtarget: { enable: false },
+			// 			recording: { enable: false },
+			// 			zoom: { enable: false },
+			// 			zoomfactor: { factor: 1 },
+			// 			dynamiczoom: { enable: false },
+			// 			dynamiczoomdirection: { direction: 'forward' },
+			// 		},
+			// 	},
+			// 	image: {
+			// 		hdr: {
+			// 			control: { control: 'off' },
+			// 		},
+			// 		af: {
+			// 			mode: { mode: 'mf' },
+			// 			trackmode: { mode: 'face' },
+			// 			motorposition: { position: 74 },
+			// 			windowcenter: { x: 0.5, y: 0.5 },
+			// 		},
+			// 		exposure: {
+			// 			mode: { mode: 'auto' },
+			// 			auto: {
+			// 				mode: { mode: 'face' },
+			// 				compensation: { evbias: 0 },
+			// 				isorange: { isomin: 100, isomax: 6400 },
+			// 				lock: { enable: false },
+			// 			},
+			// 			manual: {
+			// 				shuttertime: { shutter: '1/30' },
+			// 				iso: { iso: 100 },
+			// 			},
+			// 			antiflick: {
+			// 				mode: { mode: 'off' },
+			// 			},
+			// 		},
+			// 		whitebalance: {
+			// 			config: { mode: 'manual', temperature: 4000 },
+			// 		},
+			// 		style: {
+			// 			mode: { mode: 'standard', brightness: 50, contrast: 50, hue: 50, saturation: 50, sharpness: 50 },
+			// 		},
+			// 	},
+			// 	ptz: {
+			// 		gimbalinvert: { enable: false },
+			// 		zoom: { enable: 4 },
+			// 		preset: {
+			// 			presetList: [
+			// 				{ id: 0, pitch: 1.64, yaw: -53.3, roll: 0, ratio: 1.51, name: 'UDE=' },
+			// 				{ id: 1, pitch: -8.38, yaw: 4.29, roll: 0, ratio: 2, name: 'SGFsYm5haA==' },
+			// 				{ id: 2, pitch: -12.8, yaw: 11.9, roll: 0, ratio: 1, name: 'U3RlaHRpcw==' },
+			// 				{ id: 3, pitch: 8.93, yaw: 149, roll: 0, ratio: 1, name: 'UDQ=' },
+			// 			],
+			// 		},
+			// 	},
+			// 	status: {
+			// 		preset_cnt: [
+			// 			{
+			// 				id: 0,
+			// 				name: 'UDE=',
+			// 			},
+			// 			{
+			// 				id: 1,
+			// 				name: 'SGFsYm5haA==',
+			// 			},
+			// 			{
+			// 				id: 2,
+			// 				name: 'U3RlaHRpcw==',
+			// 			},
+			// 			{
+			// 				id: 3,
+			// 				name: 'UDQ=',
+			// 			},
+			// 		],
+			// 		ratio: 4.0,
+			// 		iszoom: false,
+			// 		target: 0,
+			// 		capture: false,
+			// 		record: false,
+			// 		af_mode: 3,
+			// 		mf_position: 74,
+			// 		is_active: true,
+			// 		ndi_available: true,
+			// 		rtmp_info: {
+			// 			rtmp_status: 0,
+			// 		},
+			// 		sdcard: {
+			// 			status: 2,
+			// 			total: 15929475072,
+			// 			available: 15713042432,
+			// 			sdcard_speed: 2,
+			// 			avail_time: 2095,
+			// 			format: 2,
+			// 		},
+			// 		device_status: {
+			// 			is_gimbal_online: true,
+			// 			is_ai_online: true,
+			// 			is_battery_online: true,
+			// 			is_lens_online: true,
+			// 			is_tof_online: true,
+			// 			is_sensor_ok: true,
+			// 			is_media_ok: true,
+			// 			is_ble_ok: true,
+			// 			is_usbwifi_attached: false,
+			// 			is_poe_attached: true,
+			// 			is_hdmi_attached: false,
+			// 			is_35mm_attached: false,
+			// 			is_remote_attached: false,
+			// 			is_charge: true,
+			// 			battery_cap: 89,
+			// 			battery_temperature: 23748,
+			// 			battery_temperature_status: 0,
+			// 			cpu_temperature: 0,
+			// 			cpu_temperature_status: 0,
+			// 			lens_temperature: 42,
+			// 			lens_temperature_status: 0,
+			// 		},
+			// 	},
 		}
 	}
 
@@ -238,13 +283,11 @@ class ObsbotRestCameraInstance extends InstanceBase {
 
 		this.ws.on('open', () => {
 			this.updateStatus(InstanceStatus.Ok)
-			this.log('debug', `Connection opened`)
 			if (this.config.reset_variables) {
 				this.updateVariables()
 			}
 		})
 		this.ws.on('close', (code) => {
-			this.log('debug', `Connection closed with code ${code}`)
 			this.updateStatus(InstanceStatus.Disconnected, `Connection closed with code ${code}`)
 		})
 
@@ -256,7 +299,7 @@ class ObsbotRestCameraInstance extends InstanceBase {
 	}
 
 	messageReceivedFromWebSocket(data) {
-		this.log('debug', `Message received: ${data}`)
+		//this.log('debug', `Message received: ${data}`)
 
 		let msgValue = null
 		if (Buffer.isBuffer(data)) {
@@ -268,24 +311,28 @@ class ObsbotRestCameraInstance extends InstanceBase {
 		} else {
 			try {
 				msgValue = JSON.parse(data)
+				//console.log(JSON.stringify(msgValue, null, 2))
 			} catch (e) {
 				msgValue = data
 			}
 		}
 	}
 
-	async sendCommand(cmd) {
+	async sendCommand(endpoint = '/', method = 'POST', data = {}) {
 		this.log('debug', 'sendCommand()')
 
 		const t = AbortSignal.timeout(5000)
 
 		const options = {
 			signal: AbortSignal.any([t, this.controller.signal]),
+			method,
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(data),
 		}
 
 		const start = Date.now()
 		try {
-			await this.getAPI(cmd, options)
+			await this.getAPI(endpoint, options)
 
 			this.updateStatus(InstanceStatus.Ok)
 		} catch (error) {
@@ -310,55 +357,33 @@ class ObsbotRestCameraInstance extends InstanceBase {
 		}
 	}
 
-	async pullData() {
-		this.log('debug', 'pullData()')
+	setDataByEndpointPath(endpoint, apiResult) {
+		const path = endpoint.split('/').filter(Boolean) // entfernt leere Strings durch führenden Slash
+
+		if (path.length && apiResult) {
+			let ref = this.data
+			for (let i = 0; i < path.length - 1; i++) {
+				if (typeof ref[path[i]] !== 'object' || ref[path[i]] === null) {
+					ref[path[i]] = {}
+				}
+				ref = ref[path[i]]
+			}
+			ref[path[path.length - 1]] = apiResult
+		}
+	}
+
+	async pollData() {
+		this.log('debug', 'pollData()')
 
 		if (this.queue.isEmpty()) {
-			//this.queue.enqueue('record')
-			//this.queue.enqueue('record/control')
-			this.queue.enqueue('record/bitrate')
-			this.queue.enqueue('record/encoder')
-			this.queue.enqueue('record/resolution')
-			this.queue.enqueue('ndi-rtsp/control')
-			this.queue.enqueue('ndi-rtsp/bitrate')
-			this.queue.enqueue('ndi-rtsp/encoder')
-			this.queue.enqueue('ndi-rtsp/resolution')
-			this.queue.enqueue('hdmi/output/resolution')
-			this.queue.enqueue('usb/mode')
-			this.queue.enqueue('audio/input/source')
-			this.queue.enqueue('audio/input/volume')
-			this.queue.enqueue('audio/input/mute')
-			this.queue.enqueue('audio/input/enc')
-			this.queue.enqueue('audio/input/agc')
-			this.queue.enqueue('ai/workmode')
-			this.queue.enqueue('ai/trackspeed')
-			this.queue.enqueue('ai/gesturecontrol/lockedtarget')
-			this.queue.enqueue('ai/gesturecontrol/recording')
-			this.queue.enqueue('ai/gesturecontrol/zoom')
-			this.queue.enqueue('ai/gesturecontrol/zoomfactor')
-			this.queue.enqueue('ai/gesturecontrol/dynamiczoom')
-			this.queue.enqueue('ai/gesturecontrol/dynamiczoomdirection')
-			this.queue.enqueue('image/hdr/control')
-			//this.queue.enqueue('image/af/mode')
-			this.queue.enqueue('image/af/trackmode')
-			//this.queue.enqueue('image/af/motorposition')
-			this.queue.enqueue('image/af/windowcenter')
-			this.queue.enqueue('image/exposure/mode')
-			this.queue.enqueue('image/exposure/auto/mode')
-			this.queue.enqueue('image/exposure/auto/compensation')
-			this.queue.enqueue('image/exposure/auto/isorange')
-			this.queue.enqueue('image/exposure/auto/lock')
-			this.queue.enqueue('image/exposure/manual/shuttertime')
-			this.queue.enqueue('image/exposure/manual/iso')
-			this.queue.enqueue('image/exposure/antiflick/mode')
-			this.queue.enqueue('image/whitebalance/config')
-			this.queue.enqueue('image/style/mode')
-			this.queue.enqueue('ptz/gimbalinvert')
-			//this.queue.enqueue('ptz/zoom')
-			this.queue.enqueue('ptz/preset')
+			console.log(JSON.stringify(this.data, null, 2))
+
+			for (const endpoint of API_ENDPOINTS) {
+				this.queue.enqueue(endpoint)
+			}
 		}
 
-		const t = AbortSignal.timeout(950)
+		const t = AbortSignal.timeout(1000)
 
 		const options = {
 			signal: AbortSignal.any([t, this.controller.signal]),
@@ -366,9 +391,11 @@ class ObsbotRestCameraInstance extends InstanceBase {
 
 		const start = Date.now()
 		try {
-			await this.getAPI(this.queue.dequeue(), options)
+			const endpoint = this.queue.dequeue()
+			const apiResult = await this.getAPI(endpoint, options)
+			this.setDataByEndpointPath(endpoint, apiResult)
 
-			this.updateStatus(InstanceStatus.Ok)
+			//this.updateStatus(InstanceStatus.Ok)
 		} catch (error) {
 			switch (error.name) {
 				case 'TimeoutError':
@@ -389,14 +416,14 @@ class ObsbotRestCameraInstance extends InstanceBase {
 			this.checkVariables()
 			this.checkFeedbacks()
 
-			if (!this.controller.signal.aborted && !this.queue.isEmpty()) {
-				this.pollID = setTimeout(() => this.pullData(), 10)
+			if (!this.controller.signal.aborted && this.pollID !== null) {
+				this.pollID = setTimeout(() => this.pollData(), 50)
 			}
 		}
 	}
 
-	async getAPI(cmd, options) {
-		const url = `http://${this.config.host}:${this.config.port}/camera/sdk/${cmd}`
+	async getAPI(endpoint, options) {
+		const url = `http://${this.config.host}:${this.config.port}/camera/sdk` + endpoint
 		this.log('debug', 'GET ' + url)
 
 		const response = await fetch(url, options)
